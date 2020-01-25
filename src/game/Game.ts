0218dd7383cc3e7f289b8103bfd18126afeac859
龙杨喜 <longames@qq.com>
2020-01-25 16:00:50 +0800

    Auto adjust position of grid

--- src/game/Game.ts
+++ src/game/Game.ts
@@ -4,6 +4,8 @@ import Grid from "./Grid";
 
 export class GameApp extends PIXI.Application {
 
+    grid: Grid;
+
     constructor(parent: HTMLElement, width: number, height: number) {
 
         super({width, height, backgroundColor : 0x000000});
@@ -12,7 +14,7 @@ export class GameApp extends PIXI.Application {
 
         // init Pixi loader
         let loader = new PIXI.Loader();
-        // Add user tile assets
+        // Add tile assets
         let tiles = assets.tiles;
         console.log('tiles to load', assets);
 
@@ -23,16 +25,23 @@ export class GameApp extends PIXI.Application {
 
         // Load assets
         loader.load(this.onAssetsLoaded.bind(this));
-    }
 
+        //update grid position when window resized
+        window.addEventListener("resize", this.onResize.bind(this), false);
+    }
+    private onResize() {
+        if(this.grid) {
+            this.grid.updatePosition();
+        }
+    }
     private onAssetsLoaded() {
 
         // let bgTexture = PIXI.Texture.from(assets.bg);
         // let bg = new PIXI.Sprite(bgTexture);
         // this.app.stage.addChild(bg);
 
-        let grid: Grid = new Grid();
-        this.stage.addChild(grid);    
+        this.grid = new Grid();
+        this.stage.addChild(this.grid);    
     }
 
 }
--- src/game/Grid.ts
+++ src/game/Grid.ts
@@ -1,10 +1,12 @@
 import * as PIXI from "pixi.js";
 import Tile from './Tile';
-import { GRID_SIZE } from "./Const";
+import { GRID_SIZE, TILE_SIZE } from "./Const";
 
 class Grid extends PIXI.Container {
     public readonly  w: number = GRID_SIZE.w;
     public readonly h: number = GRID_SIZE.h;
+    public readonly wx: number = this.w * TILE_SIZE;
+    public readonly wy: number = this.h * TILE_SIZE;
     constructor() {
         super();
         this.init();
@@ -16,6 +18,12 @@ class Grid extends PIXI.Container {
                 this.addChild(tile);
             } 
         }
+        this.updatePosition();
+    }
+    public updatePosition() {
+        let windowWidth: number = window.innerWidth;
+        let windowHeight: number = window.innerHeight;
+        this.position.set((windowWidth - this.wx)/2, (windowHeight - this.wy)/2);
     }
 }
 
